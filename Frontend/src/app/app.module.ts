aff8ca79f95ef257e0ca73bae11c94359ab766d7
Shubham Haldankar <snh16041998@gmail.com>
2023-12-03 22:12:01 +0530

    Included HttpClientModule in App Module

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ExploreComponent } from './explore/explore.component';
 import { SearchComponent } from './search/search.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { SearchComponent } from './search/search.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
