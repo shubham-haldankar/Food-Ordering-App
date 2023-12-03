52e6d0d3ff34d12c8559212ebdc95e64e2973195
Shubham Haldankar <snh16041998@gmail.com>
2023-12-03 21:54:25 +0530

    Added getRestaurants method to Restaurant Service

--- Frontend/src/app/services/restaurant.service.ts
+++ Frontend/src/app/services/restaurant.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { restaurant } from '../datatypes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
+  private rUrl: string= "http://localhost:3000/restaurants"
+  private rCUrl: string= "http://localhost:3000/restaurantCredentials"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRestaurants(){
+    return this.http.get<restaurant[]>(this.rUrl)
+  }
 }
