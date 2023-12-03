46430443b2077e3d91343518450594c7d0a08a8a
Shubham Haldankar <snh16041998@gmail.com>
2023-12-03 23:32:19 +0530

    Implemented Search Component

--- Frontend/src/app/search/search.component.ts
+++ Frontend/src/app/search/search.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { restaurant } from '../datatypes';
+import { RestaurantService } from '../services/restaurant.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+
+  searchResult: restaurant[] | undefined;
+  query:string= ''
+
+  constructor(private restaurantService: RestaurantService, private activatedRoute:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+      data=>{
+        this.query=data['query']
+        this.restaurantService.search(this.query).subscribe(result=>this.searchResult=result)
+      }
+    )
+  }
 
 }
