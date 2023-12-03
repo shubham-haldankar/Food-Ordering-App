332d5ace6d6fc7051faa89b57dcd86815bbed5d7
Shubham Haldankar <snh16041998@gmail.com>
2023-12-03 13:54:22 +0530

    Added routes to newly created components

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SearchComponent } from './search/search.component';
+import { ExploreComponent } from './explore/explore.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'search/:query', component: SearchComponent },
+  { path: 'explore/:rId', component: ExploreComponent },
+  { path: 'checkout', component: CheckoutComponent },
   { path : '**', component: PageNotFoundComponent }
 ];
 
