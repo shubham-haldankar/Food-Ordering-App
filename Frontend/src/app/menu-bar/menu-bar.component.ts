c29bc0d8a98695d62070299f1f55a3f6b86f8977
Shubham Haldankar <snh16041998@gmail.com>
2023-12-03 13:05:45 +0530

    Implemented menu bar

--- Frontend/src/app/menu-bar/menu-bar.component.ts
+++ Frontend/src/app/menu-bar/menu-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { foodItem } from '../datatypes';
 
 @Component({
   selector: 'app-menu-bar',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent {
+  searchResult: foodItem[] | undefined;
+
+  constructor(private router:Router) {}
+
+  suggestions(query:KeyboardEvent){
+    console.log(query)
+  }
+
+  hideSearch(){
+    this.searchResult= undefined;
+  }
+
+  submitSearch(searchInput:string){
+    console.log(searchInput)
+  }
+
+  explorePage(rsId:number){
+    console.log(rsId)
+  }
+
+  checkout(){
+    this.router.navigate(['/checkout'])
+  }
 
 }
