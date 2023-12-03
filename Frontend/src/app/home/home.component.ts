9ec3b80ea805ef3b4480b0e6abc59807e0ff1c78
Shubham Haldankar <snh16041998@gmail.com>
2023-12-03 22:14:14 +0530

    Injected Restaurant Service

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RestaurantService } from '../services/restaurant.service';
+import { restaurant } from '../datatypes';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  restaurants: restaurant[] | undefined;
+
+  constructor(private restaurantService:RestaurantService) {}
+
+  ngOnInit(): void {
+    this.restaurantService.getRestaurants().subscribe(restaurants=>{
+      this.restaurants=restaurants
+    })
+  }
 
 }
