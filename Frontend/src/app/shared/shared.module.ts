8b15b274c9b53c41104f3acfe27c49301b41d6b5
Shubham Haldankar <snh16041998@gmail.com>
2023-12-04 23:51:47 +0530

    Added Profile Component to Shared Module

--- Frontend/src/app/shared/shared.module.ts
+++ Frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule
