0a519030427d187eecef54cd611022c1ed73ded5
Shubham Haldankar <snh16041998@gmail.com>
2023-12-04 22:53:50 +0530

    Implemented Explore Component completely

--- Frontend/src/app/explore/explore.component.ts
+++ Frontend/src/app/explore/explore.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { cartItem, foodItem } from '../datatypes';
 import { ActivatedRoute } from '@angular/router';
 import { RestaurantService } from '../services/restaurant.service';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-explore',
@@ -18,23 +19,33 @@ export class ExploreComponent implements OnInit {
   discountedPrice: number= 0;
   successMessage: string= "";
   errorMessage: string= "";
+  cartArray:  cartItem[]= [];
 
-  constructor(private activatedRoute:ActivatedRoute, private restaurantService:RestaurantService) { }
+  constructor(private activatedRoute:ActivatedRoute, private restaurantService:RestaurantService,  private cartService: CartService) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(data=>{
-      this.rsId=Number(data['rId'])
-      this.restaurantService.getRestaurantDetails(this.rsId).subscribe(
-        data=>{
-          this.foodArray= data.menu
-          this.restaurantName= data.name
-        }
-      )
-    })
+    this.activatedRoute.params.subscribe(
+      data=>{
+        this.rsId=Number(data['rId'])
+        this.restaurantService.getRestaurantDetails(this.rsId).subscribe(
+          data=>{
+            this.foodArray= data.menu
+            this.restaurantName= data.name
+          }
+        )
+      }
+    )
+    this.cartService.cart$.subscribe(data=>data?this.cartArray=data:this.cartArray=[])
   }
 
   selectItem(food: foodItem){
     this.selectedItem= food;
+    var itemIndex = this.cartArray.filter(i=>i['restaurant-id']==this.rsId).map(i=>i.name).indexOf(this.selectedItem.name);//use id instead of name(for uniqueness)
+    if(itemIndex!=-1){
+      this.selectedQuantity= this.cartArray[itemIndex].quantity;
+    }else{
+      this.selectedQuantity= 1;
+    }
     this.discountedPrice= this.selectedItem.price*(100-this.selectedItem.discount)/100;;
     this.selectedTotal= this.discountedPrice*this.selectedQuantity;
   }
@@ -70,7 +81,12 @@ export class ExploreComponent implements OnInit {
   }
 
   updatingCart(){
-    
+    if(this.selectedItem){
+      let item: any;
+      let st= this.discountedPrice*this.selectedQuantity;
+      item= {...this.selectedItem, rsId: this.rsId, quantity: this.selectedQuantity, subTotal: st}
+      this.cartService.updateCart(item)
+    }
   }
 
 }
