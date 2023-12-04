99177dd8becd943ef5c8704f03b75eea5daf5620
Shubham Haldankar <snh16041998@gmail.com>
2023-12-04 23:52:08 +0530

    Added Sign Up Component to Shared Module

--- Frontend/src/app/shared/shared.module.ts
+++ Frontend/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    ProfileComponent
+    ProfileComponent,
+    SignUpComponent
   ],
   imports: [
     CommonModule
