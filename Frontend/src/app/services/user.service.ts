db99584e1843d39561960e9550ad0c80f2551981
Shubham Haldankar <snh16041998@gmail.com>
2023-12-05 14:07:45 +0530

    Added getCart and verify methods to User Service

--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { credentials } from '../datatypes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private usersUrl: string= "http://localhost:3000/users"
+  private ucUrl: string= "http://localhost:3000/userCredentials"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  async getCart(email:string){
+    let user= await this.http.get<any>(this.ucUrl+`?email=${encodeURIComponent(email)}`).toPromise()
+    let userDetails= await this.http.get<any>(this.usersUrl+`/${user[0].id}`).toPromise()
+    return userDetails.cart
+  }
+
+  async verify(email:string, password:string):Promise<number|null>{
+    let user= await this.http.get<credentials[]>(this.ucUrl+`?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`).toPromise()
+    if(user==undefined){
+      user= []
+    }
+    return user.length? user[0].id: null
+  }
+
 }
