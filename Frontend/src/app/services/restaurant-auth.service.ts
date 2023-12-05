0929b84cbf0b9d59eadb5bce335ae7a29be77457
Shubham Haldankar <snh16041998@gmail.com>
2023-12-05 14:05:42 +0530

    Added login(), logout() to Restaurant Auth Service

--- Frontend/src/app/services/restaurant-auth.service.ts
+++ Frontend/src/app/services/restaurant-auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { RestaurantService } from './restaurant.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantAuthService {
 
-  constructor() { }
+  constructor(private restaurantService:RestaurantService, private router: Router) { }
+
+  async login(e:string, p:string){
+    let verified: boolean= Boolean(await this.restaurantService.verify(e,p))
+    console.log('this is login',verified)
+    if(verified){
+      localStorage.setItem('email',e)
+      localStorage.setItem('password',p)
+      verified= true;
+    }
+    return verified;
+  }
+
+  logout(){
+    localStorage.removeItem('email')
+    localStorage.removeItem('password')
+    this.router.navigate(['/'])
+  }
+
 }
