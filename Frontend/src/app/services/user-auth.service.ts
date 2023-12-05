91c357096ee67a5b212aac8f27c6b59b2de7df84
Shubham Haldankar <snh16041998@gmail.com>
2023-12-05 14:08:23 +0530

    Added login(), logout() to User Auth Service

--- Frontend/src/app/services/user-auth.service.ts
+++ Frontend/src/app/services/user-auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { UserService } from './user.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserAuthService {
 
-  constructor() { }
+  constructor(private user: UserService, private router: Router) { }
+
+  async login(e:string, p:string){
+    let verified: boolean= Boolean(await this.user.verify(e,p))
+    console.log('this is login',verified)
+    if(verified){
+      localStorage.setItem('email',e)
+      localStorage.setItem('password',p)
+      verified= true;
+    }
+    return verified;
+  }
+
+  logout(){
+    localStorage.removeItem('email')
+    localStorage.removeItem('password')
+    this.router.navigate(['/'])
+  }
 }
