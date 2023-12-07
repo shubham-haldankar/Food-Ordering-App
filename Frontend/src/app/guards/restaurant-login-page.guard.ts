a1100af69a74484a887e662af9c7ae822afb77ce
Shubham Haldankar <snh16041998@gmail.com>
2023-12-07 15:04:37 +0530

    Implemented restaurantLoginPage Guard

--- Frontend/src/app/guards/restaurant-login-page.guard.ts
+++ Frontend/src/app/guards/restaurant-login-page.guard.ts
@@ -1,5 +1,25 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { RestaurantAuthService } from '../services/restaurant-auth.service';
 
-export const restaurantLoginPageGuard: CanActivateFn = (route, state) => {
+export const restaurantLoginPageGuard: CanActivateFn = async (route, state) => {
+  const restaurantAuthService:RestaurantAuthService= inject(RestaurantAuthService)
+  const router:Router= inject(Router)
+  let e= localStorage.getItem('email')
+  let p= localStorage.getItem('password')
+  if(!e){
+    e= ''
+  }
+  if(!p){
+    p=''
+  }
+  if(e && p){
+    if(await restaurantAuthService.login(e,p)){
+      router.navigate(['/restaurants'])
+      return false
+    }else{
+      return true
+    }
+  }
   return true;
 };
