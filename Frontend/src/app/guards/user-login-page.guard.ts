31d8ce83ac49a6d140ca1facf2883b22532f1db8
Shubham Haldankar <snh16041998@gmail.com>
2023-12-07 14:44:35 +0530

    Implemented userLoginPage Guard

--- Frontend/src/app/guards/user-login-page.guard.ts
+++ Frontend/src/app/guards/user-login-page.guard.ts
@@ -1,5 +1,25 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { UserAuthService } from '../services/user-auth.service';
 
-export const userLoginPageGuard: CanActivateFn = (route, state) => {
+export const userLoginPageGuard: CanActivateFn = async (route, state) => {
+  const userAuth:UserAuthService= inject(UserAuthService)
+  const router:Router= inject(Router)
+  let e= localStorage.getItem('email')
+  let p= localStorage.getItem('password')
+  if(!e){
+    e= ''
+  }
+  if(!p){
+    p=''
+  }
+  if(e && p){
+    if(await userAuth.login(e,p)){
+      router.navigate(['/'])
+      return false
+    }else{
+      return true
+    }
+  }
   return true;
 };
