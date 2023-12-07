63900d8664ebe3eb0a483006782ffaaa3199e5fa
Shubham Haldankar <snh16041998@gmail.com>
2023-12-07 14:56:00 +0530

    Implemented restaurantAuth Guard

--- Frontend/src/app/guards/restaurant-auth.guard.ts
+++ Frontend/src/app/guards/restaurant-auth.guard.ts
@@ -1,5 +1,29 @@
-import { CanActivateChildFn } from '@angular/router';
+import { CanActivateChildFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { RestaurantAuthService } from '../services/restaurant-auth.service';
 
-export const restaurantAuthGuard: CanActivateChildFn = (childRoute, state) => {
-  return true;
+export const restaurantAuthGuard: CanActivateChildFn = async (childRoute, state) => {
+  const restaurantAuth:RestaurantAuthService= inject(RestaurantAuthService)
+  const router:Router= inject(Router)
+  let e= localStorage.getItem('email')
+  let p= localStorage.getItem('password')
+  if(!e){
+    e= ''
+  }
+  if(!p){
+    p=''
+  }
+
+  if(e && p){
+    console.log(e,p)
+    if(await restaurantAuth.login(e,p)){
+      return true
+    }else{
+      router.navigate(['restaurants/login'])
+      return false
+    }
+  }
+  
+  router.navigate(['restaurants/login'])
+  return false;
 };
